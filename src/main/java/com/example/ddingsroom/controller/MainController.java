adf4e95d8e32f11f61491b62fffac42a781c40b3
choieuntaek <choiet0225@gmail.com>
2025-01-29 15:27:27 +0900

    feat: jwt login feature

---
+++ src/main/java/com/example/ddingsroom/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.example.ddingsroom.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+@ResponseBody
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainP() {
+
+        // 이름 가져오기
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        // 역할 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "main Controller: " + name + " role: "+ role;
+    }
+}
