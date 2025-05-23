e291da2bb9488e02ba792d7d440592862ee9cfbc
choieuntaek <choiet0225@gmail.com>
2025-05-23 20:41:04 +0900

    [Feat] Join Process update

--- src/main/java/com/example/ddingsroom/user/controller/JoinController.java
+++ src/main/java/com/example/ddingsroom/user/controller/JoinController.java
@@ -1,6 +1,5 @@
 package com.example.ddingsroom.user.controller;
 
-import com.example.ddingsroom.reservation.dto.*;
 import com.example.ddingsroom.user.dto.*;
 import com.example.ddingsroom.user.service.JoinService;
 import org.springframework.http.ResponseEntity;
@@ -10,33 +9,38 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value="/user", produces="application/json; charset=utf8")
 public class JoinController {
     private final JoinService joinService;
+
     public JoinController(JoinService joinService) {
         this.joinService = joinService;
     }
+
     @GetMapping("/test")
-    public String test(){
-        return "test";
+    public ResponseEntity<ResponseDTO> test(){
+        return ResponseEntity.ok(new ResponseDTO("테스트 API가 정상적으로 작동합니다."));
     }
+
     @PostMapping("/join")
-    public String joinProcess(JoinDTO joinDTO) {
-        System.out.println(joinDTO.getUsername());
-        joinService.joinProcess(joinDTO);
-        return "ok";
+    public ResponseEntity<ResponseDTO> joinProcess(@RequestBody JoinDTO joinDTO) {
+        return joinService.joinProcess(joinDTO);
     }
+
     @PostMapping("/modify-password")
-    public ResponseEntity<String> modifyPassword(@RequestBody ModifyPasswordDTO modifyPasswordDTO) {
+    public ResponseEntity<ResponseDTO> modifyPassword(@RequestBody ModifyPasswordDTO modifyPasswordDTO) {
         return joinService.modifyPassword(modifyPasswordDTO);
     }
+
     @PostMapping("/code-send")
-    public ResponseEntity<String> codeSend(@RequestBody CodeSendDTO codeSendDTO) {
+    public ResponseEntity<ResponseDTO> codeSend(@RequestBody CodeSendDTO codeSendDTO) {
         return joinService.autentication1(codeSendDTO);
     }
+
     @PostMapping("/code-verify")
-    public ResponseEntity<String> codeVerify(@RequestBody CodeVerifyDTO codeVerifyDTO) {
+    public ResponseEntity<ResponseDTO> codeVerify(@RequestBody CodeVerifyDTO codeVerifyDTO) {
         return joinService.verifyCode(codeVerifyDTO);
     }
+
     @PostMapping("/sign-up")
-    public ResponseEntity<String> signUp(@RequestBody SignUpDTO signUpDTO) {
+    public ResponseEntity<ResponseDTO> signUp(@RequestBody SignUpDTO signUpDTO) {
         return joinService.signUp(signUpDTO);
     }
 }
